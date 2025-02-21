87c630fe8083a064a2070e9c027c671f01baac3a
poo <romaissayasmine16@gmail.com>
2025-02-21 16:35:39 +0100

    ajouter generation et telechargement facture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { InvoiceModule } from './invoices/invoices.module';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'boughani',
-    password: 'boughani',
-    database: 'armac',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  }),
-  UsersModule
-],
+      type: 'mysql',
+      host: 'localhost',
+      port: 3300, // ⚠️ Port de MySQL (vérifié sur l'image)
+      username: 'root',
+      password: 'boughani', // ⚠️ Ajoute le mot de passe correct ici
+      database: 'armac',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    InvoiceModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/invoice.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from 'typeorm';
+import { Profile } from './profile.entity';
+
+
+
+export class invoice {
+    @PrimaryGeneratedColumn()
+    id?: number;
+ 
+
+    @Column({ unique: true, nullable: true })
+    name?: string;
+
+ 
+}
