83b87d40590c71fa78d98b13a26c7e52a3418050
yasminelk <romaissayasmine16@gmail.com>
2025-02-21 21:50:55 +0100

    getAll & delete & add employe

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User])] , 
-  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  exports: [UsersService], // Export for AuthService use
-
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
